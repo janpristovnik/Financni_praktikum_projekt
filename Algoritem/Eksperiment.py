a60f7bbb46f3d8b38fa04d833fd667056438ef68
janpristovnik <jan.pristovnik@gmail.com>
2018-01-05 13:14:56 +0100

    Update Eksperiment.py

--- Algoritem/Eksperiment.py
+++ Algoritem/Eksperiment.py
@@ -73,26 +73,25 @@ def randomTockeKrog(n, c):
 
 
 def je_v_elipsi(e, p):
-    "vrne True, če je točka p v elipsi e"
-    F1x, F1y = e[0]  # 1.gorišče
-    F2x, F2y = e[1]  # 2.gorišče
+    "vrne True, če je točka p v elipsi e, elipsa je podana kot par polosi a,b (a>b)"
+    a,b = e[0],e[1]
+    E = ( a**2 - b**2)**(0.5)
+    F1x, F1y = -E, 0
+    F2x, F2y = E,0
 
-    if math.hypot(F1x - p[0], F1y - p[1]) + math.hypot(F2x - p[0], F2y - p[1]) <= e[2]:
+    d = 2* ( E**2 + b**2)**(0.5)
+
+    if math.hypot(F1x - p[0], F1y - p[1]) + math.hypot(F2x - p[0], F2y - p[1]) <= d:
         return True
     else:
         return False
 
 
+
 def randomTockeElipsa(n, e):
     "Vrne n-naključnih točk znotraj elipse e s središčem v (0,0)"
-    F1x, F1y = e[0]  # 1.gorišče
-    F2x, F2y = e[1]  # 2.gorišče
-    d = e[2]
-    E = abs(F2x)
 
-    b = (d ** 2 / 4 - E ** 2) ** (0.5)
-    a = (E ** 2 + b ** 2) ** (0.5)
-
+    a, b = e[0], e[1]
     tocke = []
     minX, maxX, minY, maxY = -a, a, -b, b
 
@@ -114,8 +113,25 @@ r = 50
 
 # Pravokotnik
 Pravokotnik = [(-r,-(math.pi* r)/4),(r,-(math.pi* r)/4),(r,(math.pi* r)/4),(-r,(math.pi* r)/4)] #ploščina pravokotnika enaka kot ploščina kroga
-# Elipsa
-Elipsa = [(-r, 0), (r, 0), 3*r]
+
+Pravokotnik_dolg = [(-10000, 0), (10000, 0), (10000, 1), (-10000, 1)]
+
+Pravokotnik_običajen = [(-100, -50), (100, -50), (100, 50), (-100, 50)]
+
+Pravokotnik_kvadrat = [(-100, -99), (100, -99), (100, 99), (-100, 99)]
+
+# Elipsa_okrogla
+
+Elipsa1 = (50,49)
+
+#Elipsa_navadna
+
+Elipsa2 = (50, 35)
+
+#Elipsa_podolgovata
+
+Elipsa3 = (150, 15)
+
 # Kvadrat
 Kvadrat = [(-r, -r), (r, -r), (r, r), (-r, r)]
 # Trikotnik
@@ -125,17 +141,21 @@ Krog = (0, 0, r)
 
 
 # Eksperiment #####################################################################
-oblike1 = [Pravokotnik, Kvadrat, Trikotnik]
-imena = ["Pravokotnik", "Kvadrat", "Trikotnik"]
+#oblike1 = [Pravokotnik, Kvadrat, Trikotnik]
+#imena = ["Pravokotnik", "Kvadrat", "Trikotnik"]
 
+
+oblike_pravokotniki = [Pravokotnik_dolg, Pravokotnik_običajen, Pravokotnik_kvadrat]
+imena = ["Pravokotnik_dolg", "Pravokotnik_obicajen", "Pravokotnik_kvadrat"]
 import time
+Slovar_casov = {}
 
 def eksperiment(oblike):
     Slovar_casov = {}
     for (i, oblika) in enumerate(oblike):
         casi = []
         for n in (100, 1000,10000,100000):
-            cas_n = []            
+            cas_n = []
             for _ in range(100):
                 tocke = randomTocke(n, oblika)
                 to = time.time()
@@ -150,7 +170,7 @@ def eksperiment(oblike):
 def eksperiment_krog(c):
     casi = []
     for n in (100, 1000,10000,100000):
-        cas_n = []        
+        cas_n = []
         for _ in range(100):
             tocke = randomTockeKrog(n, c)
             to = time.time()
